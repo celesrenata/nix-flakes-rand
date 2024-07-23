2154927daa1dc45ea0c4d7af66f033b6ebc708b9
Celes Renata <celes@frameshift.net>
2024-07-23 13:13:43 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -56,5 +56,6 @@ mount -o compress=zstd,subvol=workplace ${DISK}3 /mnt/workplace
 mount ${DISK}1 /mnt/boot
 nixos-generate-config --root /mnt
 cd ${CWD}
+sudo nix-prefetch-url --name displaylink-580.zip https://www.synaptics.com/sites/default/files/exe_files/2023-08/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.8-EXE.zip
 sudo cp -r * /mnt/etc/nixos
 sudo nixos-install --root /mnt --flake /mnt/etc/nixos#rand
